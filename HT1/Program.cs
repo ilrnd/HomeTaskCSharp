28c50e6a02410596f9ccbe39379424271ca4b64b
Ilya Skripnikov <scripnikovs@list.ru>
2023-01-13 14:58:13 +0300

    Added task 4

--- HT1/Program.cs
+++ HT1/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// Console.Clear();
+// Console.Write("Введите число a: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число b: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// if (a > b)
+//   Console.Write($"Большее число равно {a}, меньшее число {b} ");
+// else if (a < b)
+//   Console.Write($"Большее число равно {b}, меньшее число {a} ");
+// else
+//   Console.Write($"Вы ввели равные числа");
+
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Clear();
+Console.Write("Введите 1-е число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-e число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 3-e число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (a == b || b == c)
+  Console.Write($"Вы ввели равные числа");
+else
+{
+if (b > max)
+  max = b;
+if (c > max)
+  max = c;
+Console.Write($"Максимальное число равно {max}");
+}
+
+
+
