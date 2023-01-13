7aebc524623616d0a54351e920f9998e1e966e31
Ilya Skripnikov <scripnikovs@list.ru>
2023-01-13 10:58:48 +0300

    Added code

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,4 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello, World!");
-int n = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(n*3);
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// //Console.WriteLine(n*3);
+// Console.WriteLine($"Результат: {n*3}");
+// Console.Clear();
+// Console.Write("Введите 1-ое число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите 2-ое число: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// if (n > m)
+//   Console.WriteLine(n);
+// else if (m > n)
+//   Console.WriteLine(m);
+// else 
+//   Console.WriteLine("=");
+
