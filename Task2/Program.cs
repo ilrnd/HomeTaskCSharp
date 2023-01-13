e27330ff1f8bac43746d4a2eb8da0eb8d579e4ea
Ilya Skripnikov <scripnikovs@list.ru>
2023-01-13 12:13:14 +0300

    Added tasks seminar

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -16,8 +16,48 @@
 //   Console.WriteLine(m);
 // else 
 //   Console.WriteLine("=");
-Console.Clear();
-Console.Write("Введите число: ");
-int n = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"Квадрат введенного числа: {n*n}");
-
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"Квадрат введенного числа: {n*n}");
+// Console.Clear();
+// Console.Write("Введите 1-е число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите 2-е число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// if(a / b == b)
+//   Console.WriteLine("Первое число является квадратом второго");
+// else
+//   Console.WriteLine("Первое число не является квадратом второго");
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// if(a == 1)
+//   Console.WriteLine("Понедельник");
+// else if(a == 2)
+//   Console.WriteLine("Вторник");
+// else if(a == 3)
+//   Console.WriteLine("Среда");
+// else if(a == 4)
+//   Console.WriteLine("Четверг");
+// else if(a == 5)
+//   Console.WriteLine("Пятница");
+// else if(a == 6)
+//   Console.WriteLine("Суббота");
+// else
+//   Console.WriteLine("Воскресенье");
+   
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int count = -n;
+// while (count < n+1)
+// {
+//   Console.Write($"{count} ");
+//   count = count +1;
+// }
+// Console.Clear();
+// Console.Write("Введите целое техзначное число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int m = n%10;
+// Console.WriteLine(m);
