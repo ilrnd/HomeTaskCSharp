7922828a18412781b8317d219deab1f977f383c7
Ilya Skripnikov <scripnikovs@list.ru>
2023-01-13 15:15:08 +0300

    Added HT1

--- HT1/Program.cs
+++ HT1/Program.cs
@@ -13,24 +13,49 @@
 
 
 // Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// Console.Clear();
+// Console.Write("Введите 1-е число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите 2-e число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите 3-e число: ");
+// int c = Convert.ToInt32(Console.ReadLine());
+// int max = a;
+// if (a == b || b == c)
+//   Console.Write($"Вы ввели равные числа");
+// else
+// {
+// if (b > max)
+//   max = b;
+// if (c > max)
+//   max = c;
+// Console.Write($"Максимальное число равно {max}");
+// }
+
+
+//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// Console.Clear();
+// Console.Write("Введите целое число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// if (a%2 == 0)
+//   Console.Write("Вы ввели четное число");
+// else
+//   Console.Write("Вы ввели нечетное число");
+
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
 Console.Clear();
-Console.Write("Введите 1-е число: ");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите 2-e число: ");
-int b = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите 3-e число: ");
-int c = Convert.ToInt32(Console.ReadLine());
-int max = a;
-if (a == b || b == c)
-  Console.Write($"Вы ввели равные числа");
-else
+Console.Write("Введите целое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+if (n > 1)
 {
-if (b > max)
-  max = b;
-if (c > max)
-  max = c;
-Console.Write($"Максимальное число равно {max}");
+  while (count <= n)
+  {
+    Console.Write($"{count} ");
+    count = count + 2;
+  }
 }
-
-
-
+else
+Console.Write($"Положительные четные числа в ряде от 1 до введенного числа отсуствуют");
