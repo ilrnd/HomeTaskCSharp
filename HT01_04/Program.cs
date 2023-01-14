321b604a97c82653345ce869abc88cbfac0044ae
Ilya Skripnikov <scripnikovs@list.ru>
2023-01-14 11:27:26 +0300

    Added HT01_04

---
+++ HT01_04/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Clear();
+Console.Write("Введите 1-е число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-e число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 3-e число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (a == b || b == c)
+  Console.Write($"Вы ввели равные числа");
+else
+{
+if (b > max)
+  max = b;
+if (c > max)
+  max = c;
+Console.Write($"Максимальное число равно {max}");
+}
