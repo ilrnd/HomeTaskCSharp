1d334e5c0bbacf98db73f270d23d3de0d98918ef
Ilya Skripnikov <scripnikovs@list.ru>
2023-01-14 11:34:15 +0300

    Added HT01_08

---
+++ HT01_08/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Clear();
+Console.Write("Введите целое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+if (n >= 2)
+{
+  while (count <= n)
+  {
+    Console.Write($"{count} ");
+    count = count + 2;
+  }
+}
+else
+Console.Write($"Положительные четные числа в ряде от 1 до введенного числа отсуствуют");
