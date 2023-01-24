7f79c3b75bdd3e400e9cd3d3758dd7611fc3dccc
Ilya Skripnikov <scripnikovs@list.ru>
2023-01-24 14:06:18 +0300

    Added HT04_27

---
+++ HT04_27/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+Console.Clear();
+Console.Write("Введите число: ");
+string a = Console.ReadLine();
+int sum = 0;
+for (int i = 0; i < a.Length; i++)
+{
+  sum = sum + Convert.ToInt32(a[i].ToString());
+}
+Console.WriteLine($"Сумма цифр равна = {sum}");
