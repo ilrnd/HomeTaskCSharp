2f4e035ebe6d949bcdadd82e5296abf138f04521
Ilya Skripnikov <scripnikovs@list.ru>
2023-01-24 20:28:52 +0300

    Added HT_04_323

---
+++ HT04_323/Program.cs
@@ -0,0 +1,32 @@
+﻿//Гипотеза Гольдбаха
+Console.Clear();
+Console.Write("Введите число от 4 до 998: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 4 || n > 998)
+{
+  Console.Write("Введено некорректное число. Введите число от 4 до 998: ");
+  n = Convert.ToInt32(Console.ReadLine());
+}
+
+bool prime(int number)
+{
+  bool numb = true;
+  for (int j = 2; j <= number; j++) // механизм проверки на простое число
+  if (number % j == 0 && number != j)
+  {
+    numb = false;
+    break;
+  }
+  return numb;
+}
+
+for (int i = 2; i < n; i++)
+{
+  bool flag1 = prime(i);
+  bool flag2 = prime(n - i);
+  if (flag1 && flag2 )
+  {
+  Console.Write($"{i} + {n-i} ");
+  break;
+  }
+}
