461e4db24371d49df5685199e84a4993c2739b5e
Ilya Skripnikov <scripnikovs@list.ru>
2023-02-03 14:44:14 +0300

    Added HT07_1236

---
+++ HT07_1236/Program.cs
@@ -0,0 +1,51 @@
+﻿// ЗАДАЧА №1236	Транспонирование - 4
+
+void FillMatrix(int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      matrix[i, j] = new Random().Next(1, 11);
+    }
+  }
+}
+
+void ScreenMatrix(int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      Console.Write($"{matrix[i, j]} \t");
+    }
+    Console.WriteLine();
+  }
+}
+
+
+void ReleaseMatrix(int[,] matrix)
+{
+  int temp = 0;
+  for (int j = 0; j < matrix.GetLength(1); j++)
+  {
+    for (int i = 0; i < matrix.GetLength(0) / 2; i++)
+    {
+      temp = matrix[i, j];
+      matrix[i, j] = matrix[matrix.GetLength(0) - 1 - i, j];
+      matrix[matrix.GetLength(0) - 1 - i, j] = temp;
+    }
+  }
+}
+
+Console.Clear();
+Console.Write("Ведите размеры массива: ");
+int[] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+FillMatrix(matrix);
+ScreenMatrix(matrix);
+Console.WriteLine();
+ReleaseMatrix(matrix);
+ScreenMatrix(matrix);
+
+
