3883e4dc81dd3a5cd638bf9d1d3ecd056d5e98e6
Ilya Skripnikov <scripnikovs@list.ru>
2023-02-08 09:53:28 +0300

    Added HT08_60

---
+++ HT08_60/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+void FillMatrix(int[,,] matrix)
+{
+  int num = 10;
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      for (int k = 0; k < matrix.GetLength(2); k++)
+      {
+        matrix[i, j, k] = num;
+        num++;
+        Console.Write($"{matrix[i, j, k]}({i},{j},{k}) \t");
+      }
+      Console.WriteLine();
+    }
+  }
+}
+Console.Clear();
+Console.Write("Ведите размеры матриц: ");
+int[] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+int[,,] matrix = new int[size[0], size[1], size[2]];
+if (size[0] * size[1] * size[2] > 90)
+{
+  Console.WriteLine("Невозможно заполнить данный массив только двухзначными числами");
+}
+else
+{
+  FillMatrix(matrix);
+}
