d65d3eedc7253aceac9eb1467ddcd1cda96bfc08
Ilya Skripnikov <scripnikovs@list.ru>
2023-02-10 12:47:47 +0300

    Added HT09_66

---
+++ HT09_66/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumNaturalNumbers(int m, int n)
+{
+  if (n == m)
+    return m;
+  return n + SumNaturalNumbers(m, n - 1);
+}
+Console.Clear();
+Console.Write("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(SumNaturalNumbers(m, n));
+
